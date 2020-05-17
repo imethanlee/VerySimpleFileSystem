d651536065405e3d0e684eb13670b92d0f59177f
Ethan Lee <Howisitgoing@yeah.net>
2020-05-16 21:50:07 -0700

    Add

--- VerySimpleFileSystem/VerySimpleFileSystem.cpp
+++ VerySimpleFileSystem/VerySimpleFileSystem.cpp
@@ -1,20 +1,147 @@
-﻿// VerySimpleFileSystem.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include "util.h"
 
-#include <iostream>
+class DiskSystem {
+public:
+	DiskSystem() {
+		/* 系统分配空间 */
+		system_start_addr = (char*)malloc(SYSTEM_SIZE * sizeof(char));
 
-int main()
-{
-    std::cout << "Hello World!\n";
+		/* 设置super_block地址 */
+		super_block = (SuperBlock*)system_start_addr;
+
+		/* 设置inode_bitmap地址 */
+		inode_bitmap = system_start_addr + OFFSET_INODE_BITMAP * BLOCK_SIZE;
+		char* tmp_inode_bitmap = new char[NUM_INODES];
+		memcpy(inode_bitmap, tmp_inode_bitmap, NUM_INODES * sizeof(char));
+		memset(inode_bitmap, '0', NUM_INODES * sizeof(char));
+		delete[] tmp_inode_bitmap;
+
+		/* 设置data_bitmap地址 */
+		data_bitmap = system_start_addr + OFFSET_DATA_BITMAP * BLOCK_SIZE;
+		char* tmp_data_bitmap = new char[NUM_BLOCKS];
+		memcpy(data_bitmap, tmp_data_bitmap, NUM_BLOCKS * sizeof(char));
+		memset(data_bitmap, '0', NUM_BLOCKS * sizeof(char));
+		delete[] tmp_data_bitmap;
+
+
+		/* 初始化i-node */
+		inodes = (INode*)(system_start_addr + OFFSET_INODES * BLOCK_SIZE);
+		INode* tmp_inodes = new INode[NUM_INODES];
+		memcpy(inodes, tmp_inodes, NUM_INODES * sizeof(INode));
+		delete[] tmp_inodes;
+
+		/* 设置data_block起始地址 */
+		data_start_addr = system_start_addr + OFFSET_DATA * BLOCK_SIZE;
+	}
+
+	~DiskSystem() {
+		// 备份系统
+
+		// 释放空间
+		free(system_start_addr);
+	}
+
+	void systemReload() {
+		// 从备份文件中读取
+	}
+
+	char* getSystemStartAddr() {
+		return system_start_addr;
+	}
+
+	void getSuperBlock() {
+
+	}
+
+	char* getDataBlockAddrFromID(int block_id) {
+		return data_start_addr + (block_id * BLOCK_SIZE);
+	}
+
+	int getDataBlockIDFromAddr(char* block_addr) {
+		return (block_addr - data_start_addr) / BLOCK_SIZE;
+	}
+
+	int getFreeINode() {
+
+	}
+
+public:
+	char* system_start_addr;	// 整个系统起始地址
+	SuperBlock* super_block;	// super block地址
+	char* inode_bitmap;			// i-node bitmap地址
+	char* data_bitmap;			// data block bitmap地址
+	INode* inodes;				// i-node地址
+	char* data_start_addr;		// data block地址
+};
+
+
+class FileManager {
+public:
+	FileManager() {
+		curr_ptr = disk.getSystemStartAddr();
+	}
+
+	~FileManager() {
+
+	}
+
+	void createFile(const char* file_name, const int file_size, const int multiplier) // Create a file
+	{
+		/* 随机生成字符串 */
+		char* str = new char[file_size * multiplier];
+		for (int i = 0; i < file_size * multiplier - 1; ++i) {
+			switch (rand() % 3)
+			{
+			case 0:
+				str[i] = '0' + rand() % 10;
+				break;
+			case 1:
+				str[i] = 'a' + rand() % 26;
+				break;
+			case 2:
+				str[i] = 'A' + rand() % 26;
+				break;
+			default:
+				cout << "create file ERROR!" << endl;
+			}
+
+		}
+		str[file_size * multiplier - 1] = '\0';
+		cout << str << endl;
+
+		/* 获取文件创建时间 */
+
+		/* 写入 */
+		// memcpy_s(a, sizeof(a), &b, 2);
+	}
+
+	void deleteFile(const char* file_name); // Delete a file
+
+	void createDirectory(const char* dir_name); // Create a directory
+
+	void deleteDirectory(const char* dir_name); // Delete a directory
+
+	void changeDirectory(const char* dir_name); // Change current directory
+
+	void listAll(); // List all the files and sub-directories under current working directory
+
+	void copyFile(); // Copy a file
+
+	void displayUsage(); // Display the usage of storage space
+
+	void printFileContents(); // 
+private:
+	DiskSystem disk;
+	char* curr_ptr;
+};
+
+
+int main() {
+	srand(time(0));
+	DiskSystem ds;
+	FileManager fm;
+
+
+
+	system("pause");
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ VerySimpleFileSystem/util.h
@@ -0,0 +1,53 @@
+#pragma once
+#include<iostream>
+#include<bitset>
+#include<random>
+#include<ctime>
+#include<string>
+using namespace std;
+
+#define SYSTEM_SIZE 16 * 1024 * 1024		// System总大小
+#define BLOCK_SIZE 1 * 1024					// Block空间大小
+#define NUM_BLOCKS 16 * 1024				// Block总个数
+#define NUM_INODES	62						// i-node总个数
+#define ADDR_PER_INODE 10					// i-node中direct的个数
+
+#define OFFSET_INODE_BITMAP 1
+#define OFFSET_DATA_BITMAP 2
+#define OFFSET_INODES 18
+#define OFFSET_DATA 24
+
+class INode {
+public:
+	char type;
+	char name[20];
+	char time_created[20];
+	int direct[ADDR_PER_INODE];
+	int indirect = 999;
+};
+
+class SuperBlock {
+public:
+	int magic;
+	int nblocks;
+	int ninodeblocks;
+	int ninodes;
+};
+
+
+string getCurrTime() {
+	time_t time_seconds = time(0);
+	struct tm now_time;
+	localtime_s(&now_time, &time_seconds);
+
+	string time = to_string(now_time.tm_year + 1900)
+		+ "/"
+		+ to_string(now_time.tm_mon + 1)
+		+ "/"
+		+ to_string(now_time.tm_mday)
+		+ " "
+		+ to_string(now_time.tm_hour) + ":"
+		+ to_string(now_time.tm_min) + ":"
+		+ to_string(now_time.tm_sec);
+	return time;
+}
